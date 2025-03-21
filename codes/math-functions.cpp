e41bb209bf9c09c33851aefd340969db2987401c
Viki Nadaraja <alligatorismyname2012@gmail.com>
2025-03-21 10:27:10 +0800

    Update math-functions.cpp

--- codes/math-functions.cpp
+++ codes/math-functions.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
-
+#include <cmath>
 
 int main(){
 	
+	double x = 3.99;
+	double y = 4;
+	double z;
 	
+	z = std::max(x,y);
+	z = std::min(x,y);
+	z = pow(2,3);
+	z = sqrt(9);
+	z = abs(-3); // turns - value to +
+	z = round(x); 
+	z = ceil(x); // round up
+	z = floor(x); // round down
+	//std::cout<<z;
 	
 	return 0;
 }
